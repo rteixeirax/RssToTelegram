8051657f10ea46afde8e9a399cd3b23f71b70950
Ricardo Teixeira <rteixxeira86@gmail.com>
2020-10-13 20:45:22 +0100

    Set date local on startup

--- src/startup.ts
+++ src/startup.ts
@@ -1,9 +1,13 @@
+import date from './tools/date';
 import checkEnvVariables from './utils/checkEnvVariables';
 import consoleWriteLine from './utils/consoleWriteLine';
 import Worker from './Worker';
 
 // Verify if all .env variables are set before continue.
 checkEnvVariables();
+
+// Set date local
+date.setLocal();
 
 // Boot message
 consoleWriteLine('Up and running..');
