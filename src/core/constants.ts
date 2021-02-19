bf421eabf3c08198182b262b15fba2642d822f1b
Ricardo Teixeira <rteixxeira86@gmail.com>
2021-02-19 18:05:31 +0000

    Update constants.ts

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,6 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+export const IS_TEST = process.env.NODE_ENV === 'test';
 
 export const DATE_LOCALE = process.env.DATE_LOCALE!;
 export const REFRESH_INTERVAL_MINUTES = process.env.REFRESH_INTERVAL_MINUTES!;
